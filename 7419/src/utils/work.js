81d182ac764a37912d0d18bfe456383c91c56506
justwink <rollawaypoint@gmail.com>
2020-12-30 14:58:37 +0800

    ConcurrencyUploadService

--- 7419/src/utils/work.js
+++ 7419/src/utils/work.js
@@ -2,6 +2,22 @@ import { get, post } from './http'
 import { PUT_PHOTOS_API, GET_PHOTOS_API, SIGN_API } from './api'
 import { ls } from './ls'
 import { LOCAL_STORAGE_LOGIN_KEY } from './const'
+import { message } from 'antd'
+
+export const concurrencyUploadService = async formList => {
+  while (formList.length) {
+    const queue = formList.splice(0, 3)
+    const count = queue.length
+
+    await Promise.all(queue.map(uploadPhotoAPI))
+      .then(() => {
+        message.success(`Number of successful concurrent uploads: ${count}!`)
+      })
+      .catch(() => {
+        message.error(`Number of failed concurrent uploads: ${count}.`)
+      })
+  }
+}
 
 export const isLoginService = async data => {
   const local = data || ls.get(LOCAL_STORAGE_LOGIN_KEY)
